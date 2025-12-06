8875a6181abfc992f491fd34f4e7ca7812d58a13
Scott Fenton <sfenton3@users.noreply.github.com>
2025-12-06 06:47:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,17 @@ function App() {
 
   // Container: Defines the grid's theme & dimensions.
   return (
-    <div className='mx-auto w-400 h-100'>
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-        defaultColDef={defaultColDef}
-      />
+    <div className='p-8'>
+      <button className="mb-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-colors shadow-md">
+        Hello World
+      </button>
+      <div className='ag-theme-quartz mx-auto h-200 w-450'>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+        />
+      </div>
     </div>
   );
 };
