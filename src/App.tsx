b1a659d9442bf3c57b9709a85b7345912f11cb13
Scott Fenton <sfenton3@users.noreply.github.com>
2025-12-06 06:28:11 -0500

    init table

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import { useState } from 'react'
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-
+import React, { StrictMode, useState } from "react";
+//import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  ModuleRegistry.registerModules([AllCommunityModule]);
+  //ModuleRegistry.registerModules([AllCommunityModule]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState([
+    { field: "Name" },
+    { field: "Price" },
+    { field: "Dust Value" },
+    { field: "Dust / Chaos" },
+    { field: "Dust / Chaos / Slot" },
+    { field: "Gold Fee" },
+    { field: "Trade Link" },
+  ]);
+
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { make: "Fiat", model: "500", price: 15774, electric: false },
+    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+  ]);
 
 
+  const defaultColDef = {
+    flex: 1,
+  };
+
+  // Container: Defines the grid's theme & dimensions.
   return (
-    <>
-      <div className=''> Hello world! </div>
-    </>
-  )
-}
+    <div className='mx-auto w-300 h-100'>
+      <div style={{ width: "100%", height: "100%" }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+        />
+      </div></div>
+  );
+};
+
+
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import './index.css'
 import App from './App.tsx'
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
