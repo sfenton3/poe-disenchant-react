bb6e3965a8295c9b73f77c0d41eb6d60885a07d4
Scott Fenton <sfenton3@users.noreply.github.com>
2025-12-06 22:41:01 -0500

    get slider working

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import mockData from "./data/mockData.json";
 import { Slider } from "radix-ui";
-import "./styles.css";
-
-
 
 function App() {
   // Column Definitions: Defines & controls grid columns.
@@ -32,21 +29,13 @@ function App() {
     flex: 1,
   };
 
-  // console.log(Slider);
+  console.log(Slider);
 
   // Container: Defines the grid's theme & dimensions.
   return (
     <div className='flex flex-col gap-4'>
 
       { /** Toolbar */}
-      <div className="h-12 w-12">
-        <Slider.Root className="SliderRoot" defaultValue={[50]} max={100} step={1}>
-          <Slider.Track className="SliderTrack">
-            <Slider.Range className="SliderRange" />
-          </Slider.Track>
-          <Slider.Thumb className="SliderThumb" aria-label="Volume" />
-        </Slider.Root>
-      </div>
       <div className="flex flex-row gap-2 mx-auto w-[80%] mt-5">
         <button
           onClick={filterAmount}
@@ -57,7 +46,16 @@ function App() {
           onClick={reset}
           className="p-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-colors shadow-md">
           Reset
-        </button></div>
+        </button>
+        <div className="  border-2">
+          <Slider.Root className="relative flex items-center select-none touch-none w-[200px] h-5" defaultValue={[50]} max={100} step={1}>
+            <Slider.Track className="bg-black/30 relative flex-grow rounded-full h-[3px]">
+              <Slider.Range className="absolute bg-blue-500 rounded-full h-full" />
+            </Slider.Track>
+            <Slider.Thumb className="block w-5 h-5 bg-white shadow-lg rounded-full border-2 border-blue-500 hover:bg-purple-200 focus:outline-none focus:shadow-[0_0_0_5px_rgba(0,0,0,0.2)]" aria-label="Volume" />
+          </Slider.Root>
+        </div>
+      </div>
 
       { /** Table */}
       <div className='mx-auto w-[80vw] h-[80vh]'>
