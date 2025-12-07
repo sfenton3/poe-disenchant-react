d719e27f2623d8134c471986e0d83c8fe734c349
Scott Fenton <sfenton3@users.noreply.github.com>
2025-12-06 20:42:53 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { AgGridReact } from "ag-grid-react";
 import mockData from "./data/mockData.json";
 
 function App() {
-  //ModuleRegistry.registerModules([AllCommunityModule]);
-
   // Column Definitions: Defines & controls grid columns.
   const [colDefs, setColDefs] = useState([
     { field: "Name" },
@@ -18,6 +16,13 @@ function App() {
 
   const [rowData, setRowData] = useState(mockData);
 
+  const filterAmount = () => {
+    setRowData(mockData.filter((item) => item["Dust Value"] > 2500))
+  };
+
+  const reset = () => {
+    setRowData(mockData);
+  }
 
   const defaultColDef = {
     flex: 1,
@@ -26,8 +31,15 @@ function App() {
   // Container: Defines the grid's theme & dimensions.
   return (
     <div className='p-8'>
-      <button className="mb-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-colors shadow-md">
-        Hello World
+      <button
+        onClick={filterAmount}
+        className="mb-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-colors shadow-md">
+        Dust Range
+      </button>
+      <button
+        onClick={reset}
+        className="mb-4 ml-2 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-colors shadow-md">
+        Reset
       </button>
       <div className='ag-theme-quartz mx-auto h-200 w-450'>
         <AgGridReact
